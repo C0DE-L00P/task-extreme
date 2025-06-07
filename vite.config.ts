6bea66885ac34fc265064809aabb50e5cac1a644
Mohamed Gamal <mohamedgyaseen@gmail.com>
2025-06-07 23:47:09 +0300

    Handle 404 after deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  appType: "mpa",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          react: ["react", "react-dom"],
+          "react-router": ["react-router-dom"],
+          tailwind: ["tailwindcss"],
+        },
+      },
+    },
+  },
   plugins: [react(), tailwindcss()],
-})
+});
